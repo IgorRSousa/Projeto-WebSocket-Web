1f889f030d13e7787a19717990e124afd100650d
Igor Sousa <igorrafaelsb248@gmail.com>
2024-01-17 12:12:15 -0300

    Criação do sistema de token de acesso

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
+Route::post('/auth/ClearAuth', [AuthController::class, 'RevogarAcesso']);
+
+Route::middleware(['UserAuth'])->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
 });
